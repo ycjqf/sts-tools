2b25e822f76e8b704a25fe2fd918b0385826895d
xiaoyang <76817515+ycjqf@users.noreply.github.com>
2022-06-27 03:24:54 +0800

    fix(template): eslint react issue.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/jsx-runtime",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
